e63d5ae3c07e8aa202bd842e5fc490a2079bc5ed
Iqbal Fauzi <iqbalfawz@gmail.com>
2020-03-01 22:17:22 +0700

    Add q field

--- modules/slideshow/config.php
+++ modules/slideshow/config.php
@@ -2,7 +2,7 @@
 
 return [
     '__name' => 'slideshow',
-    '__version' => '0.0.1',
+    '__version' => '0.0.2',
     '__git' => 'git@github.com:getmim/slideshow.git',
     '__license' => 'MIT',
     '__author' => [
--- modules/slideshow/model/Slideshow.php
+++ modules/slideshow/model/Slideshow.php
@@ -14,5 +14,5 @@ class Slideshow extends \Mim\Model
 
     protected static $chains = [];
 
-    protected static $q = [];
+    protected static $q = ['name'];
 }
